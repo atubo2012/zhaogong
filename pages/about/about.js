b4d6c181d8920e641e5b20fa38004c05f87d3e38
x1 <aaa@aaa.com>
2018-05-21 14:34:30 +0800

    feat:内容可通过数组定制

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,16 +1,39 @@
 let app = getApp();
-let common = require('../../common.js');
-let cf = require('../../config.js');
 let ut = require('../../utils/utils.js');
 
 Page({
     data: {
         pageInfo: app.getPageInfo('about'),
 
+        //TODO:可以从数据库中获取
+        contents:[
+            {title:'一、最新动态',content:[
+                {title:'新上业务',content:[
+                    {title:'临时保洁',content:''},
+                    {title:'家电清洗',content:''},
+                ]}
+            ]},
+            {title:'二、发布说明',content:[
+                {title:'20180521',content:[
+                    {title:'FEAT',content:''},
+                    {title:'BFIX',content:''},
+                    {title:'UPDT',content:'内容在这里非常非常的多'},
+                ]}
+            ]},
+            {title:'三、关于我们',content:[
+                {title:'理念',content:[
+                    {title:'成就自己、影响他人',content:'   内容在这里非常非常的多内容在这里非常非常的多内容在这里非常非常的多内容在这里非常非常的多内容在这里非常非常的多内容在这里非常非常的多'},
+                ]},
+                {title:'愿景',content:[
+                    {title:'一切皆有可能',content:'内容在这里非常非常的多内容在这里非常非常的多内容在这里非常非常的多内容在这里非常非常的多内容在这里非常非常的多内容在这里非常非常的多'},
+                ]}
+            ]}
+        ]
     },
-    goHome : function (e) {
-        ut.debug(e);
+    goHome : function () {
         app.goHome();
+    },
+    onLoad:function(){
     }
 
 });
