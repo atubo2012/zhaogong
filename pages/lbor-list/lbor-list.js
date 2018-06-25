8f2b2975174978d0f62869ea8a4b9bd642336a93
x1 <aaa@aaa.com>
2018-06-25 17:59:11 +0800

    CC accept test

---
+++ pages/lbor-list/lbor-list.js
@@ -0,0 +1,28 @@
+let ut = require('../../utils/utils.js');
+const app = getApp();
+let cf = app.globalData.cf;
+
+Page({
+
+    data: {
+        pageInfo: app.getPageInfo('lbor-list'),
+        list: [],
+        cf:cf
+    },
+
+    onLoad: function () {
+        let cclist = this.selectComponent("#cc_list");
+        cclist.onLoad();
+    },
+
+    onReachBottom: function () {
+        let cclist = this.selectComponent("#cc_list");
+        cclist.onReachBottom();
+    },
+
+    onPullDownRefresh: function () {
+        let cclist = this.selectComponent("#cc_list");
+        cclist.onPullDownRefresh();
+    },
+
+});
